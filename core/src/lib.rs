e3cd0cae6693d6db9e2c016c5f1cef60af0c923d
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2023-05-30 18:26:08 +0200

    breaking: Rename `trait Mailer` to `trait DynMailer`, add `trait Mailer`

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,19 +1,33 @@
 //! Core trait for `async_mailer`. Use `async_mailer` instead.
-use std::fmt::Debug;
+use std::{fmt::Debug, sync::Arc};
 
 pub use async_trait::async_trait;
 
 pub use mail_send;
 use mail_send::smtp::message::Message;
 
-/// Type-erased mailer error, for use of [`Mailer`] as trait object.
-pub type MailerError = Box<dyn std::error::Error + Send + Sync + 'static>;
+// == Mailer ==
 
-/// Object-safe [`Mailer`] trait, usable as `Arc<dyn Mailer>` or `Box<dyn Mailer>`.
 #[async_trait]
 pub trait Mailer: Debug + Send + Sync {
-    async fn send_mail(&self, message: Message<'_>) -> Result<(), MailerError>;
+    type Error;
+
+    async fn send_mail(&self, message: Message<'_>) -> Result<(), Self::Error>;
 }
+
+// == DynMailer ==
+
+/// Type-erased mailer error, for use of [`DynMailer`] as trait object.
+pub type DynMailerError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+/// Object-safe [`DynMailer`] trait, usable as [`ArcMailer`] (`Arc<dyn DynMailer>`) or `BoxMailer` (`Box<dyn DynMailer>`).
+#[async_trait]
+pub trait DynMailer: Debug + Send + Sync {
+    async fn send_mail(&self, message: Message<'_>) -> Result<(), DynMailerError>;
+}
+
+pub type BoxMailer = Box<dyn DynMailer>;
+pub type ArcMailer = Arc<dyn DynMailer>;
 
 pub mod util {
     use super::Message;
